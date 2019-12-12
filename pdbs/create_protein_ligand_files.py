134541eb9d8cce18fa4618f15ec57ae7be89b06a
mlugowska <magdalena.lugowska@outlook.com>
2019-12-12 23:09:07 +0100

    create dirs if not exist

--- pdbs/create_protein_ligand_files.py
+++ pdbs/create_protein_ligand_files.py
@@ -12,7 +12,7 @@ def create_ligand_file(file, complex_file, complex):
     if not complex.ligand.file:
         ligand_lines = [line for line in complex_file if line[0:6] == 'HETATM' and line[17:20] != 'HOH']
         ligand_filename = os.path.join(settings.MEDIA_ROOT, 'ligands', f'{os.path.splitext(file)[0]}_ligand.sdf')
-        with open(ligand_filename, 'w') as ligand_file:
+        with open(ligand_filename, 'w+') as ligand_file:
             ligand_file.writelines(ligand_lines)
 
         complex.ligand.file = ligand_filename
@@ -22,8 +22,8 @@ def create_ligand_file(file, complex_file, complex):
 def create_protein_file(file, complex_file, complex):
     if not complex.protein.file:
         protein_lines = [line for line in complex_file if line[0:4] == 'ATOM']
-        protein_filename = os.path.join(settings.MEDIA_ROOT, 'proteins', f'{os.path.splitext(file)[0]}_protein.sdf')
-        with open(protein_filename, 'w') as protein_file:
+        protein_filename = os.path.join(settings.MEDIA_ROOT, 'proteins', f'{os.path.splitext(file)[0]}_protein.pdb')
+        with open(protein_filename, 'w+') as protein_file:
             protein_file.writelines(protein_lines)
 
         complex.protein.file = protein_filename
@@ -34,8 +34,16 @@ def split_complex_file():
     os.chdir(PATH)
     files = os.listdir(PATH)
 
+    if not os.path.exists(os.path.join(settings.MEDIA_ROOT, 'ligands')):
+        os.makedirs(os.path.join(settings.MEDIA_ROOT, 'ligands'))
+
+    if not os.path.exists(os.path.join(settings.MEDIA_ROOT, 'proteins')):
+        os.makedirs(os.path.join(settings.MEDIA_ROOT, 'proteins'))
+
     for file in files:
         complex = Complex.objects.get(pdb_id=os.path.splitext(file)[0])
         with open(file, 'r') as complex_file:
-            create_ligand_file(file, complex_file, complex)
             create_protein_file(file, complex_file, complex)
+
+        with open(file, 'r') as complex_file:
+            create_ligand_file(file, complex_file, complex)
