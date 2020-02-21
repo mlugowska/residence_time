8650208eaa3213b666e2f40ea938d23c4933994d
Magdalena Ługowska <magdalena.lugowska@outlook.com>
2020-02-21 14:04:41 +0100

    Convert pdb to sdf (#6)
    * add openbable installation instruction
    
    * remove unused swagger

--- web/residence_time/urls.py
+++ web/residence_time/urls.py
@@ -15,13 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_swagger.views import get_swagger_view
-
-schema_view = get_swagger_view(title='Residence Time API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('molecules.urls', namespace='molecules')),
     path('user/', include('users.urls', namespace='users')),
-    path('api/docs/', schema_view)
 ]
