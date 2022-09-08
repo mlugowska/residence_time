c7673aa7e71cf2eaa13e14ac85642abba772254e
mlugowska <magdalena.lugowska@outlook.com>
2022-09-08 16:10:45 +0200

    add get-started and how-to-search section & create urls

--- web/help/urls.py
+++ web/help/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path
 
 # importing views from views..py
-from help.views import get_started_view
+from help.views import get_started_view, how_to_search_view, explore_entry_view
 
 app_name = 'help'
 
 urlpatterns = [
     path('get-started/', get_started_view, name='get_started'),
-    path('how-to-search/', get_started_view, name='how_to_search'),
-    path('explore-entry/', get_started_view, name='explore_entry'),
+    path('how-to-search/', how_to_search_view, name='how_to_search'),
+    path('explore-entry/', explore_entry_view, name='explore_entry'),
 ]
